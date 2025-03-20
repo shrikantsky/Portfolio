af64e12d08491870d5fc43e003d1490e9c3ea08e
Shrikant Yadav <82shrikantyadav@gmail.com>
2025-03-20 12:37:31 +0530

    fixed added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-
 import './App.css';
 
-
-import{
+import {
   BrowserRouter,
   Routes,
   Route
-}from 'react-router-dom';
+} from 'react-router-dom';
 
 import HomePage from './component/HomePage';
 import Project from './component/Project';
 import Work from './component/Work';
 
-
-
-
-
-
 function App() {
   return (
-
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
-
-      <Route path='/photo' element={<Project/>}></Route>
-
-      <Route path='/work' element={<Work/>}/>
-    </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/photo' element={<Project />} />
+        <Route path='/work' element={<Work />} />
+      </Routes>
     </BrowserRouter>
-
-  
-    
   );
 }
 
